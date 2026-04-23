4b3e8afbaf44ec764460e8e8e871540916e24d2e
k.bando <k.bando@net.email.ne.jp>
2026-04-23 14:59:35 +0900

    commit

--- timesync/bin/Release/run_timesync.bat
+++ timesync/bin/Release/run_timesync.bat
@@ -3,5 +3,6 @@ eco "### #t#n" >>%LOG%
 timesync >> %LOG%
 if %errorlevel% neq 0 (
   eco "rc=1 (to be adjusted)#n" >>%LOG%
+  start "" cscript c:\wintool\SendKey.js timesync as1113{ENTER}
   start "timesync" runas /user:Administrator "timesync -sync"
 )
