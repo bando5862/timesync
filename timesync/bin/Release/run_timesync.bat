64d8d178f9b83b15c2d065fede8105548afd1c81
k.bando <k.bando@net.email.ne.jp>
2026-05-06 19:36:51 +0900

    commit

--- timesync/bin/Release/run_timesync.bat
+++ timesync/bin/Release/run_timesync.bat
@@ -2,5 +2,6 @@ set LOG=c:\wintool\timesync.log
 eco "### #t#n" >>%LOG%
 timesync >> %LOG%
 if %errorlevel% neq 0 (
-  call actual_timesync
+ rem バッチを管理者権限で実行
+ powershell start-process  actual_timesync.bat -verb runas
 )
